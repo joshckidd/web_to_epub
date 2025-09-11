38bd7d1aa24298885fd063325b7a762f6efa5774
joshckidd <josh@larameekidd.com>
2025-09-11 10:06:10 -0400

    early tests parsing with beautifulsoup

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+# Use this for doc reference: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
 def main():
-    print("Hello from web-to-epub!")
+    URL = "http://strangehorizons.com/wordpress/poetry/watching-migrations/"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find(class_="post")
+    category = results.find(rel="category").contents[0]
+    author = results.find(class_="author").find("a").contents[0]
+    content = results.find(class_="content")
+    content.script.decompose()
+    print(category)
+    print(author)
+    print(content)
 
 
 if __name__ == "__main__":
