19efc4a42b48d4a1b9707d867e24f8413347543f
joshckidd <josh@larameekidd.com>
2025-09-18 10:18:09 -0400

    added link pages, fixed parsing multiple

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from parse import get_values, get_links
 
 def main():
     settings_dict = get_settings()
-    # update for parsing links out of a page
     links = get_links(settings_dict["sources"])
     for url in links:
         values = get_values(url, settings_dict["values"])
--- parse.py
+++ parse.py
@@ -17,6 +17,11 @@ def get_links(sources):
     links = []
     if "links" in sources:
         links += sources["links"]
+    if "link-pages" in sources:
+        for link_page in sources["link-pages"]:
+            page = requests.get(link_page["url"])
+            soup = BeautifulSoup(page.content, "html.parser")
+            links += parse_soup(soup, link_page["pattern"])
     return links
 
 def parse_soup(soup, value_rule):
@@ -28,7 +33,13 @@ def parse_soup(soup, value_rule):
         return list(map(lambda x: str(x), results))
     elif value_rule_split[1] == "text":
         return list(map(lambda x: x.get_text(), results))
-    return parse_soup(results[0], value_rule_split[1])
+    elif value_rule_split[1][0:5] == "attr=":
+        split_attr = value_rule_split[1].split("=")
+        return list(map(lambda x: x.attrs[split_attr[1]], results))
+    new_results = []
+    for result in results:
+        new_results += parse_soup(result, value_rule_split[1])
+    return new_results
 
 def remove_tags(content, tag_list):
     soup = BeautifulSoup(content, "html.parser")
